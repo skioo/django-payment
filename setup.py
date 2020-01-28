701f0b1400a0671b183673784eadd1b00a750440
Nicholas Wolff <nwolff@gmail.com>
2020-01-28 12:10:53 +0100

    release 1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-payment',
-    version='1.7',
+    version='1.8',
     description='',
     long_description='',
     author='Nicholas Wolff',
